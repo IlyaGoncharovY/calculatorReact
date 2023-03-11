5b01e856ee5e1acdbf0d56f5e499c6e4b635bb66
IlyaGoncharovY <goncharov.i@rambler.ru>
2023-03-11 19:03:21 +0300

    fix (test example) delete test example

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import {Action, combineReducers, configureStore, ThunkAction} from '@reduxjs/toolkit';
+import calculatorReducer from "../BLL/calculatorSlice"
+
+
+const rootReducer = combineReducers({
+  calculatorReducer
+})
 
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
+  reducer: rootReducer
 });
 
 export type AppDispatch = typeof store.dispatch;
